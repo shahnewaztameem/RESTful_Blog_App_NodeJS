31ab16fa335343d64332a06f78f90ac3be5e3b28
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-03-16 01:34:48 +0600

    basic setup and read data from DB

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-var express     = require('express'),
-    body_parser = require('body-parser'),
-    mongoose    = require('mongoose'),
-    app         = express();
+var express = require('express'),
+bodyParser = require('body-parser'),
+mongoose    = require('mongoose'),
+app         = express();
+
+//db connection config
+mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true });
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended:true }));
+
+//blog Schema config
+var blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+// RESTful routes
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (error, blogs) => {
+        if(error) {
+            console.log(error);
+        }
+        else {
+            res.render('index',{blogs: blogs});
+        }
+    });
+    
+});
+
+app.listen(3000, () => {
+    console.log("Server is runnig on port 3000");
+})
